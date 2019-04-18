abae6147a307ef2501bea9f5942111af23e7a3ed
Nikita <nikitsidenk@mail.ru>
2019-04-18 03:11:08 +0300

    update dependences and webpack config

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App';
 
 
-
-const elem = (
-    <h1>
-        HEllo
-    </h1>
-)
 
 render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('app'),
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,43 @@
 const path = require('path');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-
 const config = {
-    entry: [
-        './src/index.js',
-        './src/scss/index.scss'
-    ],
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: '[name].js',
-        publicPath: '/dist/' // по этому пути папка dist доступна в браузере
-    },
-    devServer: {
-        contentBase: path.join(__dirname, '/dist'),
-        overlay: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                loader: 'babel-loader',
-                exclude: [/node_modules/],
-            },
-            {
-                test: /\.scss$/,
-                // use: [
-                //     'style-loader', добавляет css в js файл, а затем в head
-                //     'css-loader',
-                // ]
-                use: ExtractTextPlugin.extract({
-                    use: ['css-loader', 'sass-loader'],
-                })
-            }
-        ]
-    },
-    resolve: {
-            extensions: ['.scss', '.js'],
-            modules: ['node_modules', 'src'],
-    },
-    devtool: 'source-map', // карта сайта для корректного отображения кода в консоли браузера
-    plugins: [
-        new ExtractTextPlugin('styles.css') // создает в дист файл styles.css со всеми стилями
+  entry: [
+    './src/index.js',
+    './src/scss/index.scss'
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+    publicPath: '/dist'
+  },
+  devServer: {
+    overlay: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: [/node_modules/],
+      },
+      {
+        test: /\.scss$/,
+     
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader', 'sass-loader'],
+        })
+      }
     ]
+  },
+  resolve: {
+    extensions: ['.scss', '.js'],
+    modules: ['node_modules', 'src'],
+  },
+  devtool: 'source-map',
+  plugins: [
+    new ExtractTextPlugin('styles.css')
+  ]
 }
 
 module.exports = config;
